02e6377a8f7bacf7fcbda9e6b6bbbbd920fab065
Dragos Guta <contact.dragosguta@gmail.com>
2016-01-28 23:32:31 -0800

    Landing page is up

---
+++ Gemfile
@@ -0,0 +1,84 @@
+source 'https://rubygems.org'
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.5'
+# Use postgresql as the database for Active Record
+gem 'pg', '~> 0.15'
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 5.0'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails', '~> 4.1.0'
+# See https://github.com/rails/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.0'
+# bundle exec rake doc:rails generates the API under doc/api.
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+# HAML
+gem 'haml', '~> 4.0', '>= 4.0.7'
+
+# Resizing/editing images
+gem 'mini_magick', '~> 4.3', '>= 4.3.6'
+
+# Uploading to Amazon Web Services
+gem 'carrierwave', '~> 0.10.0'
+gem 'fog', '~> 1.37'
+
+gem 'unf', '~> 0.1.4'
+
+# Managing Environment
+gem 'figaro', '~> 1.1', '>= 1.1.1'
+gem 'fog-aws', '~> 0.8.1'
+
+# Materialize
+gem 'materialize-sass', '~> 0.97.5'
+
+# Authentication/Devise
+gem 'devise', '~> 3.5', '>= 3.5.5'
+
+# Font Awesome
+gem 'font-awesome-rails', '~> 4.5'
+
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use Unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+end
+
+group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+
+  # Capistrano Set Up
+  gem 'capistrano',           require: false
+  gem 'capistrano-rvm',       require: false
+  gem 'capistrano-rails',     require: false
+  gem 'capistrano-bundler',   require: false
+  gem 'capistrano-passenger', require: false
+end
+
+group :production do
+  gem 'rails_12factor'
+end
+
---
+++ app/assets/javascripts/application-all.js
@@ -0,0 +1,111 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree ./all
+
+(function($) {
+
+  skel
+    .breakpoints({
+      xlarge: '(max-width: 1680px)',
+      large:  '(max-width: 1280px)',
+      medium: '(max-width: 980px)',
+      small:  '(max-width: 736px)',
+      xsmall: '(max-width: 480px)'
+    });
+
+  $(function() {
+
+    var $window = $(window),
+      $body = $('body'),
+      $wrapper = $('#page-wrapper'),
+      $banner = $('#banner'),
+      $header = $('#header');
+
+    // Disable animations/transitions until the page has loaded.
+      $body.addClass('is-loading');
+
+      $window.on('load', function() {
+        window.setTimeout(function() {
+          $body.removeClass('is-loading');
+        }, 100);
+      });
+
+    // Mobile?
+      if (skel.vars.mobile)
+        $body.addClass('is-mobile');
+      else
+        skel
+          .on('-medium !medium', function() {
+            $body.removeClass('is-mobile');
+          })
+          .on('+medium', function() {
+            $body.addClass('is-mobile');
+          });
+
+    // Fix: Placeholder polyfill.
+      $('form').placeholder();
+
+    // Prioritize "important" elements on medium.
+      skel.on('+medium -medium', function() {
+        $.prioritize(
+          '.important\\28 medium\\29',
+          skel.breakpoint('medium').active
+        );
+      });
+
+    // Scrolly.
+      $('.scrolly')
+        .scrolly({
+          speed: 1500,
+          offset: $header.outerHeight()
+        });
+
+    // Menu.
+      $('#menu')
+        .append('<a href="#menu" class="close"></a>')
+        .appendTo($body)
+        .panel({
+          delay: 500,
+          hideOnClick: true,
+          hideOnSwipe: true,
+          resetScroll: true,
+          resetForms: true,
+          side: 'right',
+          target: $body,
+          visibleClass: 'is-menu-visible'
+        });
+
+    // Header.
+      if (skel.vars.IEVersion < 9)
+        $header.removeClass('alt');
+
+      if ($banner.length > 0
+      &&  $header.hasClass('alt')) {
+
+        $window.on('resize', function() { $window.trigger('scroll'); });
+
+        $banner.scrollex({
+          bottom:   $header.outerHeight() + 1,
+          terminate:  function() { $header.removeClass('alt'); },
+          enter:    function() { $header.addClass('alt'); },
+          leave:    function() { $header.removeClass('alt'); }
+        });
+
+      }
+  });
+
+})(jQuery);
+
